9cd09ff175018614d48b67c2df68822bb0d42615
sbd021 <sbd021@126.com>
2020-03-11 10:00:40 +0800

    modify ps script

--- pre_build_event.ps1
+++ pre_build_event.ps1
@@ -1,1 +1,2 @@
-Invoke-WebRequest -Uri "https://download.agora.io/sdk/release/Agora_Native_SDK_for_Windows(x86)_v2_9_3_FULL.zip" -OutFile .\AgoraSDK.zip;
+param($SDKUrl)
+Invoke-WebRequest -Uri $SDKUrl -OutFile .\AgoraSDK.zip;
